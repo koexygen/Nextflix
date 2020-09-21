6251ee7b54e238a3389d470b39258bd549da8365
Koexygen <rexarkoexygen@gmail.com>
2020-09-21 22:44:34 +0400

    test

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,13 +1,12 @@
+# frozen_string_literal: true
+
 class Api::SessionsController < ApplicationController
   def create
     @user = User.find_by_credentials(
       params[:user][:username],
       params[:user][:password]
     )
-
-
   end
 
   def destroy; end
-
 end
