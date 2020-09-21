6353c99d07f74096056b07f04b56881a63571ed9
Koexygen <rexarkoexygen@gmail.com>
2020-09-22 02:14:12 +0400

    completed session_reducers.js

---
+++ frontend/reducers/session_reducers.js
@@ -0,0 +1,22 @@
+import {
+  RECEIVE_CURRENT_USER,
+  RECEIVE_SESSION_ERRORS,
+  LOGOUT_USER,
+} from "../actions/session_action";
+
+export const sessionReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return Object.assign({}, state, { id: action.user.id });
+    case LOGOUT_USER:
+      return { id: null };
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
