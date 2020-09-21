3afe9533da37eefb9a10f0750843d6e3aff466b3
Koexygen <rexarkoexygen@gmail.com>
2020-09-22 01:21:54 +0400

    before heroku

---
+++ frontend/actions/session_action.js
@@ -0,0 +1,44 @@
+import * as AuthUtil from "../util/auth_util";
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_USER = "LOGOUT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+export const receiveCurrentUser = (user) => ({
+  type: RECEIVE_CURRENT_USER,
+  user,
+});
+
+export const logoutCurrentUser = () => ({ type: LOGOUT_USER });
+
+export const receiveSessionError = (errors) => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors,
+});
+
+export const loginUser = (user) => (dispatch) => {
+  return AuthUtil.login(user).then(
+    (respUser) => {
+      return dispatch(receiveCurrentUser(respUser));
+    },
+    (error) => dispatch(receiveSessionError(error))
+  );
+};
+
+export const signupUser = (user) => (dispatch) => {
+  return AuthUtil.signup(user).then(
+    (respUser) => {
+      return dispatch(receiveCurrentUser(respUser));
+    },
+    (error) => dispatch(receiveSessionError(error))
+  );
+};
+
+export const logoutUser = () => (dispatch) => {
+  return AuthUtil.logout().then(
+    () => {
+      return dispatch(logoutCurrentUser());
+    },
+    (error) => dispatch(receiveSessionError(error))
+  );
+};
