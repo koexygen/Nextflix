2f3deaac67784a6ea41f50f16faef62dce4bf5cd
Koexygen <rexarkoexygen@gmail.com>
2020-09-22 20:27:47 +0400

    Built Landing page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import HeaderNavContainer from "./header/Header-navContainer";
+import LandingPage from "./LandingPage";
 
 const App = () => {
-  return <HeaderNavContainer />;
+  return <LandingPage />;
 };
 
 export default App;
---
+++ frontend/components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderNav from "./header/Header-navContainer";
+
+export default class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <HeaderNav />;
+  }
+}
