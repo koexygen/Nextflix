c1e85f16bb0d952c72e559a060afff60a1d762cb
Koexygen <rexarkoexygen@gmail.com>
2020-09-22 18:36:21 +0400

    bootstrapping current user done, deleted window's current user authentication works, moving to components

--- frontend/nextflix.jsx
+++ frontend/nextflix.jsx
@@ -5,8 +5,20 @@ import configureStore from "./store/store";
 import { loginUser, logoutUser } from "./actions/session_action";
 
 document.addEventListener("DOMContentLoaded", () => {
+  let store;
   const root = document.getElementById("root");
-  const store = configureStore();
+  if (window.currentUser) {
+    const preLoadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser },
+      },
+      session: { id: window.currentUser.id },
+    };
+    store = configureStore(preLoadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   window.dispatch = store.dispatch;
   window.state = store.getState;
   window.login = loginUser;
