249366f3ae04c2ed16c2bccdf2aa496f42c00409
Koexygen <rexarkoexygen@gmail.com>
2020-09-25 22:35:19 +0400

    fixed assignment of hidden div in sliderr item and moving AWS

--- frontend/components/sliders/Slider.jsx
+++ frontend/components/sliders/Slider.jsx
@@ -7,9 +7,12 @@ export default class Slider extends React.Component {
         <ul className="container">
           <li className="slider-item">1</li>
           <li className="slider-item">2</li>
-          <li className="slider-item">3</li>
+          <li className="slider-item">
+            3<div className="slider-sub-item">Sub Item</div>
+          </li>
           <li className="slider-item">4</li>
           <li className="slider-item">5</li>
+          <li className="slider-item">6</li>
         </ul>
       </div>
     );
