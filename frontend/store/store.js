ec529f79aa65daad0b2d715ae7e9590e5b9339ad
Koexygen <rexarkoexygen@gmail.com>
2020-09-28 06:44:52 +0400

    test1

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/root_reducer";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import thunk from "redux-thunk";
 
 const configureStore = (preLoadedState = {}) => {
-  return createStore(
-    rootReducer,
-    preLoadedState,
-    applyMiddleware(thunk, logger)
-  );
+  return createStore(rootReducer, preLoadedState, applyMiddleware(thunk));
 };
 
 export default configureStore;
